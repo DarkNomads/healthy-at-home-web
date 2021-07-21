e1cba329d7c466f7ab9f41fd5016270c9c9c6ddd
DarkNomads <ghostnomadjr@gmail.com>
2021-07-21 15:21:44 -0400

    Moved homepage into its own component, added angular routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' }
+  // { path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
